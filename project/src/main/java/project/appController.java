d1187301b55ca17351b9aa8595186aba13db105c
erlenmom <erlenmom@stud.ntnu.no>
2019-09-05 19:19:52 +0200

    Added correct FXML objects to the controller

--- project/src/main/java/project/appController.java
+++ project/src/main/java/project/appController.java
@@ -3,16 +3,22 @@ package project;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class appController {
 	
-	@FXML private AnchorPane anchor;
-	@FXML private Label header, player_label, player_label1, score_label, score_value, output, loss_label, leaderboard_label;
-	@FXML private TextField player_input;
-	@FXML private TextArea score_area;
-	@FXML private Button green, red, yellow, blue, start_btn, check_btn, next_round_btn;
-
+	@FXML private AnchorPane loginPane;
+	@FXML private SplitPane splitPane;
+	@FXML private Label inboxLabel, welcomeLabel, emailLabel;
+	@FXML private TextField emailField;
+	@FXML private PasswordField passwordField;
+	@FXML private TextArea textArea;
+	@FXML private Button loginButton;
+	
+	
+	
 }
