4dc1735261a934afdb9df463352a6e50f2583c24
erlenmom <erlenmom@stud.ntnu.no>
2019-09-06 18:43:24 +0200

    Added a loadData() method to appIO + added a loginCheck() method to the controller + Added a .txt document for users

--- project/src/main/java/project/appController.java
+++ project/src/main/java/project/appController.java
@@ -1,5 +1,8 @@
 package project;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,16 +17,43 @@ public class appController {
 	
 	@FXML private AnchorPane loginPane;
 	@FXML private SplitPane splitPane;
-	@FXML private Label inboxLabel, welcomeLabel, emailLabel;
+	@FXML private Label inboxLabel, welcomeLabel, emailLabel, errorLabel;
 	@FXML private TextField emailField;
 	@FXML private PasswordField passwordField;
 	@FXML private TextArea textArea;
 	@FXML private ListView inbox;
 	@FXML private Button loginButton;
 	
-	private void login() {
+	private appIO io = new appIO(); 
+	
+	private void loginVisibility() {
 		loginPane.setVisible(false);
 		splitPane.setVisible(true);
+	}
+	
+	private void loginCheck() {
+		String emailInput = emailField.getText();
+		String passwordInput = passwordField.getText();
+		ArrayList<String> users = new ArrayList<String>();
+		try {
+			users = io.loadData("users.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < users.size(); i++) {
+			String user = users.get(i);
+			String email = user.substring(0, user.indexOf(" "));
+			String password = user.substring(user.indexOf("/t"), -1);
+			if (email.equals(emailInput) && password.equals(passwordInput)) {
+				loginVisibility();
+				break;
+			}
+			else {
+				errorLabel.setVisible(true);
+			}
+		}
+		
 	}
 	
 	
--- project/src/main/java/project/appIO.java
+++ project/src/main/java/project/appIO.java
@@ -1,5 +1,35 @@
 package project;
 
-public class appIO {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class appIO {
+	
+	public ArrayList<String> loadData(String filename) throws FileNotFoundException {
+		FileReader content = new FileReader(filename);
+		LineNumberReader count = new LineNumberReader(content);
+		int result = count.getLineNumber() + 1;
+		try {
+			while (count.readLine() != null){
+				result++;
+			    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		ArrayList<String> table = new ArrayList<>();
+		Scanner scanner = new Scanner(new File(filename));
+		for (int i = 0; i < result-1; i++) {
+			table.add(scanner.nextLine().toString());
+		}
+		scanner.close();
+		return table;
+		
+		
+	}
 }
