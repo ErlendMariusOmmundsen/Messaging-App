33a7bb35692e27a0217976682f5c09f0d145c3c6
AleksanderWestergaar <aleksawk@stud.ntnu.no>
2019-09-20 16:14:48 +0200

    Working on AccountSerializer and -deserializer

---
+++ project/src/main/java/project/json/AccountSerializer.java
@@ -0,0 +1,28 @@
+package project.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import project.Account;
+
+import java.io.IOException;
+
+public class AccountSerializer extends JsonSerializer<Account>{
+	
+	
+	private static String email = "Email";
+	private static String password = "Password";
+	
+	
+	@Override
+	public void serialize(final Account account, final JsonGenerator jsonGen, final SerializerProvider provider) throws IOException {
+		jsonGen.writeStartObject();
+		jsonGen.writeFieldName(email);
+		jsonGen.writeString(account.getMail_address());
+		jsonGen.writeFieldName(password);
+		jsonGen.writeString(account.getPassword());
+		jsonGen.writeEndObject();
+	}
+
+}
