abf9db5ed2464605eb516ed84decbdeeb66c6343
Lukas Tveiten <lukasnt@stud.ntnu.no>
2019-09-30 18:48:52 +0200

    Just added json dependency to gradle and fixed some imports.

--- project/project_restapi/src/main/java/project_restapi/AccountService.java
+++ project/project_restapi/src/main/java/project_restapi/AccountService.java
@@ -13,7 +13,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import project.Account;
+import project_core.Account;
 
 @Path(AccountService.ACCOUNT_SERVICE_PATH)
 public class AccountService {
