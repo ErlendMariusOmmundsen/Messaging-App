e32dee2f37ef7d2d3ece8d8c6a05fd7e3c36e31b
erlenmom <erlenmom@stud.ntnu.no>
2019-11-05 20:29:53 +0100

    Fixes after git conflicts

--- project/project_fxui/src/test/java/project_fxui/TestFXBase.java
+++ project/project_fxui/src/test/java/project_fxui/TestFXBase.java
@@ -1,4 +1,9 @@
+
 package project_fxui;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
 
 import org.junit.After;
 import org.junit.Before;
@@ -6,26 +11,37 @@ import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
-import javafx.stage.Stage;
 
-public abstract class TestFXBase extends ApplicationTest {
-	private FxRobot robot = new FxRobot();
-	@Before
-	public void setUpClass() throws Exception{
-		ApplicationTest.launch(FxApp.class);
-	}
-	
-	public void start(Stage stage) throws Exception{
-		stage.show();
-	}
-	
-	@After
-	public void afterEachTest() throws Exception{
-		FxToolkit.hideStage();
-		robot.release(new KeyCode[] {});
-		robot.release(new MouseButton[] {});
-	}
-	
+public abstract class TestFXBase {
+  private FxRobot robot = new FxRobot();
+  
+  /**
+   * Launches the app before the tests.
+   * @throws Exception - something isnot working.
+   */
+  @Before
+  public void setUpClass() throws Exception {
+    ApplicationTest.launch(FxApp.class);
+  }
+  
+  /**
+   * Starts the test by showing a stage.
+   * @param stage - the stage to show
+   * @throws Exception - something is not working.
+   */
+  public void start(Stage stage) throws Exception {
+    stage.show();
+  }
+  
+  /**
+   * Cleans up after each test.
+   * @throws Exception - something is not working.
+   */
+  @After
+  public void afterEachTest() throws Exception {
+    FxToolkit.hideStage();
+    robot.release(new KeyCode[] {});
+    robot.release(new MouseButton[] {});
+  }
+
 }
